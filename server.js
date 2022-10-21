96356b93cace4c97f48999b3885964f2a5e48c54
Devlev <rheingoldgalaxy@gmail.com>
2022-10-22 02:14:47 +0300

    design start

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
+  'use strict';
   // Настройки
   const setup = {port:8000}
   // Подключаем express
   const express = require ('express'); 
   // создаем приложение
   const app = express ();
+  app.use(express.static('public'));
   // Маршрутизируем GET-запрос http://ваш_сайт/test
   app.get('/', (req, res) => {    
-    res.sendFile(__dirname + "/static/index.html"); 
+    res.sendFile(__dirname + "/public/index.html"); 
   });
   // Слушаем порт и при запуске сервера сообщаем
   app.listen(setup.port, () => {
