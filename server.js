787ee12c12809abf60db5c90043e5335f98706c3
Devlev <rheingoldgalaxy@gmail.com>
2022-10-22 01:54:40 +0300

    some public folder

--- server.js
+++ server.js
@@ -6,7 +6,7 @@
   const app = express ();
   // Маршрутизируем GET-запрос http://ваш_сайт/test
   app.get('/', (req, res) => {    
-    res.send('Hello from the main page!!'); 
+    res.sendFile(__dirname + "/static/index.html"); 
   });
   // Слушаем порт и при запуске сервера сообщаем
   app.listen(setup.port, () => {
